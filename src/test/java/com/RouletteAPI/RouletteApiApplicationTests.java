e4686142de23bbbe9c352abc5662b5c2af6980ca
anfdiazme <anfdiazme@unal.edu.co>
2020-10-09 11:17:08 -0500

    Begin with spring initializr

---
+++ src/test/java/com/RouletteAPI/RouletteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RouletteAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
